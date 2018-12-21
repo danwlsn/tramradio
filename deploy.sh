e39a16820d784cde26f4ca8c1f3debcf1f4c4fc3
Danny Wilson <dannywilson32@gmail.com>
2018-12-21 16:04:06 +0000

    New travis after success deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+ARTIFACT="upload.zip"
+VERSION=$1
+
+echo "Building JS"
+yarn build
+
+
+echo "Build zip file"
+cd build
+zip ../${ARTIFACT} -r *
+cd ../
+
+echo "Uploading build"
+aws s3 cp ${ARTIFACT} s3://elasticbeanstalk-eu-west-1-847878515230/versions/tramradio/${VERSION}.zip
+sleep 2
+
+echo "Creating new application version: ${VERSION}"
+aws elasticbeanstalk create-application-version \
+    --application-name "tramradio" \
+    --version-label "${VERSION}" \
+    --source-bundle S3Bucket="elasticbeanstalk-eu-west-1-847878515230",S3Key="versions/tramradio/${VERSION}.zip" \
+
+
+echo "Updated application with version: ${VERSION}"
+aws elasticbeanstalk update-environment \
+    --environment-name "tramradio-dev" \
+    --version-label "${VERSION}"
