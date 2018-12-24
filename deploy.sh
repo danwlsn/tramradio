6b6cef4499d93433ce3fba4cb3a29efbdfad6112
Danny Wilson <dannywilson32@gmail.com>
2018-12-24 11:41:47 +0000

    new python deploy.sh and added pipenv to travis

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
+APPLICATION="tramradio-fe"
+ENVIRONMENT="dev"
+
 ARTIFACT="upload.zip"
-VERSION=$( date +"%Y%m%d%H%M" )
+BUILD_NUMBER=$( date +"%Y%m%d%H%M" )
 
 echo "Building JS"
 yarn build
 
+echo "Building reqs.txt"
+pipenv lock -r > requirements.txt
+
 
 echo "Build zip file"
-cd build
-zip ../${ARTIFACT} -r *
-cd ../
+zip ./${ARTIFACT} . -x node_modules/\* -x src/\* -x README.md -x deploy.sh -x package.json -x Pipfile* -x yarn.lock -r
 
 echo "Uploading build"
-aws s3 cp ${ARTIFACT} s3://elasticbeanstalk-eu-west-1-847878515230/versions/tramradio/${VERSION}.zip
+aws s3 cp ${ARTIFACT} s3://elasticbeanstalk-eu-west-1-847878515230/versions/${APPLICATION}/${BUILD_NUMBER}.zip
 
-echo "Creating new application version: ${VERSION}"
+echo "Creating new application version: ${BUILD_NUMBER}"
 aws elasticbeanstalk create-application-version \
     --region "eu-west-1" \
-    --application-name "tramradio" \
-    --version-label "${VERSION}" \
-    --source-bundle S3Bucket="elasticbeanstalk-eu-west-1-847878515230",S3Key="versions/tramradio/${VERSION}.zip" \
+    --application-name "${APPLICATION}" \
+    --version-label "${BUILD_NUMBER}" \
+    --source-bundle S3Bucket="elasticbeanstalk-eu-west-1-847878515230",S3Key="versions/${APPLICATION}/${BUILD_NUMBER}.zip" \
 
 
-echo "Updated application with version: ${VERSION}"
+echo "Updated application with version: ${BUILD_NUMBER}"
 aws elasticbeanstalk update-environment \
     --region "eu-west-1" \
-    --environment-name "tramradio-dev" \
-    --version-label "${VERSION}"
+    --environment-name "${APPLICATION}-${ENVIRONMENT}" \
+    --version-label "${BUILD_NUMBER}"
